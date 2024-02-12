10311004473ba4aebe38e7a96be383382cb44f0a
Tess647 <thanni_taslihah@yahoo.com>
2024-02-12 10:01:01 +0100

    done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,6 +94,33 @@ class Rectangle(Base):
         for i in range(self.height):
             rectangle += (" " * self.x) + ("#" * self.width) + "\n"
         print(rectangle, end="")
+    
+    def update(self, *args, **kwargs):
+        '''
+            Updates the arguments in the class
+        '''
+        if len(args) == 0:
+            for key, val in kwargs.items():
+                self.__setattr__(key, val)
+            return
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+        except IndexError:
+            pass
+
+    def to_dictionary(self):
+        '''
+            Returns a dictionary representation of this class
+        '''
+        return {'x': getattr(self, "x"),
+                'y': getattr(self, "y"),
+                'id': getattr(self, "id"),
+                'height': getattr(self, "height"),
+                'width': getattr(self, "width")}
 
     @staticmethod
     def setter_validation(attribute, value):
@@ -102,7 +129,7 @@ class Rectangle(Base):
         if attribute == "x" or attribute  == "y":
             if value < 0:
                 raise ValueError("{} must be >= 0".format(attribute))
-            elif value <= 0:
+        elif value <= 0:
                 raise ValueError("{} must be > 0".format(attribute))
             
     def __str__(self):
